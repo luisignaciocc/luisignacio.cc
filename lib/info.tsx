667beb0fd29ffb38beeece302a378953e53cdfb8
Luis Ignacio <luisignacioccp@gmail.com>
2023-03-29 12:42:55 -0300

    Update info.tsx

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,17 +5,14 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey there! I'm Ignacio, a software developer with several years of
-      experience building products and working with startups.
+      ¡Que tal! Soy Ignacio, un desarrollador de software con varios años de experiencia construyendo productos y trabajando con startups.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I love creating technology that helps businesses succeed and thrive.
-      Whether it's coding up a slick new feature or troubleshooting a pesky bug,
-      I'm always up for a good challenge.
+      Me encanta crear tecnología que ayuda a las empresas a tener éxito y prosperar. Ya sea programando una nueva feature impresionante o solucionando un molesto bug, siempre estoy dispuesto a enfrentar un buen desafío.
     </>
   );
 };
